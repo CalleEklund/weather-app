5b8d81e3a65b955fd7bd2c223dec4809c563a56a
norparn <eklund.calle@gmail.com>
2019-10-22 19:56:00 +0200

    added a header, search bar and button. Also made the button log the value of the search bar

---
+++ weather-app/src/header.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./header.css";
+
+class header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchBox: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log(this.state.searchBox);
+  }
+  handleChange(e) {
+    this.setState({ searchBox: e.target.value });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="title">Weather info</h1>
+        <div className="searchBar">
+          <input
+            type="text"
+            name="searchBar"
+            value={this.state.searchBox}
+            onChange={this.handleChange}
+          />
+          <button className="searchBtn" onClick={this.handleSubmit} />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default header;
---
+++ weather-app/src/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header";
+import "./main.css";
+function main() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default main;
