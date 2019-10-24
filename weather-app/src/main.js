051230e666497a96077587d1f8f3cd3ea90fa812
norparn <eklund.calle@gmail.com>
2019-10-24 22:05:52 +0200

    added the weather card to the main page

--- weather-app/src/main.js
+++ weather-app/src/main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
+import Weathercard from "./weathercard"
 import "./main.css";
 function main() {
   return (
     <React.Fragment>
       <Header />
+      <Weathercard />
     </React.Fragment>
   );
 }
